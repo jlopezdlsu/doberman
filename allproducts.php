9011f028c66621744dda16323d2d5162255c9ce3
phalange865 <phalange865@gmail.com>
2019-11-03 21:31:27 +0800

    yey

---
+++ allproducts.php
@@ -0,0 +1,223 @@
+<?php
+include('conn.php'); //Database Connection
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Product Filter</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link href="css/jquery-ui.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <?php include('header.php') ?>
+  <!-- Page Content -->
+  <div class="container pt-5">
+
+    <br/>
+    <br/>
+    <div align="right" class="top_search">
+      <input type="text" class="search_input" id="search_query" name="search_query" placeholder="Advanced Search"/>
+      <input id="search_button" type="button" value="Search" class="search_bt" name="search"/>
+    </div>
+    <br/>
+    <br/>
+
+
+    <div class="row">
+      <div class="col-md-3">
+        <div class="list-group">
+          <h3>Price</h3>
+          <input type="hidden" id="min_price_hide" value="0" />
+          <input type="hidden" id="max_price_hide" value="2000" />
+          <p id="price_show">$100 - $2000</p>
+          <div id="price_range"></div>
+        </div>
+
+        <div class="list-group">
+          <h3>Category</h3>
+          <div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+            <?php
+            $query = "SELECT * FROM tbl_category ORDER BY categoryName ASC";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            foreach($result as $row)
+            {
+              ?>
+              <div class="list-group-item checkbox">
+                <label>
+                  <input type="checkbox" class="filter_all category" value="<?php echo $row['categoryID']; ?>">
+                  <?php echo $row['categoryName']; ?>
+                </label>
+              </div>
+              <?php
+            }
+            ?>
+          </div>
+        </div>
+
+        <div class="list-group">
+          <h3>Brand</h3>
+          <div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+            <?php
+
+            $query = "SELECT * FROM tbl_brand ORDER BY brandName ASC";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            foreach($result as $row)
+            {
+              ?>
+              <div class="list-group-item checkbox">
+                <label>
+                  <input type="checkbox" class="filter_all brand" value="<?php echo $row['brandID']; ?>">
+                  <?php echo $row['brandName']; ?>
+                </label>
+              </div>
+              <?php
+            }
+
+            ?>
+          </div>
+        </div>
+
+        <div class="list-group">
+          <h3>RAM</h3>
+          <div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+            <?php
+
+            $query = "
+            SELECT * FROM tbl_product WHERE ram <> '0' AND status = '1'ORDER BY ram DESC
+            ";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            foreach($result as $row)
+            {
+              ?>
+              <div class="list-group-item checkbox">
+                <label>
+                  <input type="checkbox" class="filter_all ram" value="<?php echo $row['ram']; ?>">
+                  <?php echo $row['ram']; ?>
+                </label>
+              </div>
+              <?php
+            }
+
+            ?>
+          </div>
+        </div>
+
+        <div class="list-group">
+          <h3>Internal Storage</h3>
+          <div style="height: 180px; overflow-y: auto; overflow-x: hidden;">
+            <?php
+            $query = "
+            SELECT * FROM tbl_product WHERE storage <> '0' AND status = '1' ORDER BY storage DESC
+            ";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            foreach($result as $row)
+            {
+              ?>
+              <div class="list-group-item checkbox">
+                <label><input type="checkbox" class="filter_all storage" value="<?php echo $row['storage']; ?>"  > <?php echo $row['storage']; ?> GB</label>
+              </div>
+              <?php
+            }
+            ?>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div class="col-md-9">
+
+        <div class="row filter_data">
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+  <?php include('footer.php') ?>
+  <script src="js/jquery-1.11.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <script src="js/jquery-ui.js"></script>
+
+  <script>
+  $(document).ready(function() {
+
+    filter_data();
+
+    function filter_data() {
+      $('.filter_data');
+      var action = 'fetch_data';
+      var search_query = $('#search_query').val();
+      var minimum_price = $('#min_price_hide').val();
+      var maximum_price = $('#max_price_hide').val();
+      var category = get_filter('category');
+      var brand = get_filter('brand');
+      var ram = get_filter('ram');
+      var storage = get_filter('storage');
+      $.ajax({
+        url: "fetch_data.php",
+        method: "POST",
+        data:{action:action, search_query:search_query, minimum_price:minimum_price, maximum_price:maximum_price, category:category, brand:brand, ram:ram, storage:storage},
+        success: function(data) {
+          $('.filter_data').html(data);
+        }
+      });
+    }
+
+    function get_filter(class_name) {
+      var filter = [];
+      $('.' + class_name + ':checked').each(function() {
+        filter.push($(this).val());
+      });
+      return filter;
+    }
+
+    $('.filter_all').click(function() {
+      filter_data();
+    });
+
+    $('#price_range').slider({
+      range:true,
+      min:100,
+      max:2000,
+      values:[100, 2000],
+      step:10,
+      stop: function(event, ui) {
+        $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+        $('#min_price_hide').val(ui.values[0]);
+        $('#max_price_hide').val(ui.values[1]);
+        filter_data();
+      }
+    });
+
+    $("#search_button").click(function(){
+      filter_data();
+    });
+
+  });
+  </script>
+
+</body>
+
+</html>
--- fetch_data.php
+++ fetch_data.php
@@ -1,83 +1,102 @@
 <?php
+include('conn.php'); //Database Connection
 
-//fetch_data.php
-
-include('db.php');
-
-if(isset($_POST["action"]))
-{
-	$query = "
-		SELECT * FROM product WHERE prd_status = '1'
+function getImage($img,$connect){
+	$query2 = "
+	SELECT * FROM tbl_productimg WHERE productID = '$img'
 	";
-	
+	$statement = $connect->prepare($query2);
+	$statement->execute();
+	$result = $statement->fetchAll();
+	return $result[0]["imgName"];
+}
+
+
+
+if (isset($_POST["action"])) {
+	$query = "SELECT * FROM tbl_product WHERE status = '1'";
+
 	if(isset($_POST["search_query"]) && $_POST["search_query"] !== "")
 	{
 		$catsearch=  $_POST["search_query"];
 		$query .= "
-		 AND prd_cat LIKE '".$catsearch."'";
+		AND shortDescription LIKE '%".$catsearch."%'
+		";
 	}
 	if(isset($_POST["minimum_price"], $_POST["maximum_price"]) && !empty($_POST["minimum_price"]) && !empty($_POST["maximum_price"]))
 	{
 		$query .= "
-		 AND prd_price BETWEEN '".$_POST["minimum_price"]."' AND '".$_POST["maximum_price"]."'
+		AND price BETWEEN '".$_POST["minimum_price"]."' AND '".$_POST["maximum_price"]."'
 		";
 	}
 	if(isset($_POST["category"]))
 	{
 		$cat_filter = implode("','", $_POST["category"]);
 		$query .= "
-		 AND prd_cat IN('".$cat_filter."')
+		AND categoryID IN('".$cat_filter."')
 		";
 	}
-	
+
 	if(isset($_POST["brand"]))
 	{
 		$brand_filter = implode("','", $_POST["brand"]);
 		$query .= "
-		 AND prd_brand IN('".$brand_filter."')
+		AND brandID IN('".$brand_filter."')
 		";
 	}
 	if(isset($_POST["ram"]))
 	{
 		$ram_filter = implode("','", $_POST["ram"]);
 		$query .= "
-		 AND prd_ram IN('".$ram_filter."')
+		AND ram IN('".$ram_filter."')
 		";
 	}
 	if(isset($_POST["storage"]))
 	{
 		$storage_filter = implode("','", $_POST["storage"]);
 		$query .= "
-		 AND prd_storage IN('".$storage_filter."')
+		AND storage IN('".$storage_filter."')
 		";
 	}
 
 	$statement = $connect->prepare($query);
 	$statement->execute();
-	$result = $statement->fetchAll();
+	$result    = $statement->fetchAll();
 	$total_row = $statement->rowCount();
-	$output = '';
-	if($total_row > 0)
-	{
-		foreach($result as $row)
-		{
+	$output    = '';
+	if ($total_row > 0) {
+		foreach ($result as $row) {
 			$output .= '
-			<div class="col-sm-4 col-lg-3 col-md-3">
-				<div style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; height:400px;">
-					<img src="image/'. $row['prd_image'] .'" alt="" class="img-responsive" >
-					<p align="center"><strong><a href="#">'. $row['prd_name'] .'</a></strong></p>
-					<h4 style="text-align:center;" class="text-danger" >₱'. $row['prd_price'] .'</h4>
-				</div>
+			<div class="col-md-4 col-sm-6">
+			<div class="product-grid">
+			<div class="product-image4">
+			<a href="product.php?p='. $row['productID'] .'">
+			<img class="pic-1" src="image/' . getImage($row['productID'],$connect) . '">
+			<img class="pic-2" src="image/' . getImage($row['productID'],$connect) . '">
+			</a>
+			<ul class="social">
+			<li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+			<li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+			<li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+			</ul>
+			<span class="product-new-label">New</span>
+			<span class="product-discount-label">-10%</span>
 			</div>
-			';
+			<div class="product-content">
+			<h3 class="title"><a href="product.php?p='. $row['productID'] .'">' . $row['productName'] . '</a></h3>
+			<div class="price">
+			$' . $row['price'] . '
+			<span>$' . $row['price'] . '</span>
+			</div>
+			<a class="add-to-cart" href="">ADD TO CART</a>
+			</div>
+			</div>
+			</div>';
 		}
-	}
-	else
-	{
-		$output = '<center><h3>No Data Found</h3></center>';
+	} else {
+		$output = '<h3>No Data Found</h3>';
 	}
 	echo $output;
 }
 
-//'.$query.'
 ?>
