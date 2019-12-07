1e644e5e73d226365975a3fd54f2e3e30f8ce6fa
Jason Lopez <jeysownlopez@gmail.com>
2019-12-07 14:30:13 +0800

    yey

--- conn.php
+++ conn.php
@@ -1,9 +1,8 @@
-
 <?php
- $host = "localhost";
- $db_user = "root";
- $db_pass = "";
- $dbname = "db_doberman2";
+$host = "35.240.223.49:3306";
+$db_user = "dobermandb";
+$db_pass = "wKue77tk0ovftrik";
+$dbname = "db_doberman";
 
  $connect = new PDO("mysql:host=$host; dbname=$dbname", $db_user, $db_pass);
 ?>
