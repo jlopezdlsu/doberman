67ed6c01ea9a14322e9961a742c6c8b7b0b1006f
Jason Lopez <jeysownlopez@gmail.com>
2019-12-11 11:46:18 +0800

    yey

--- header.php
+++ header.php
@@ -37,8 +37,10 @@ else{
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom box-shadow fixed-top">
     <div class="container">
       <div class="row">
-        <h3 class="my-0 mr-md-auto font-weight-normal">
-          <a href="index.php">Doberman</a>
+        <h3 class="my-0 mr-md-auto font-weight-normal col-lg-2">
+          <a href="index.php">
+            <img src="assets/images/doberman.png" alt="" style="width:50%">
+          </a>
         </h3>
         <nav class="my-2 my-md-0 mr-md-3">
           <a class="p-2 text-dark" href="allproducts.php">Products</a>
